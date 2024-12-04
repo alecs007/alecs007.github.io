7ffca9565776714b2c9fe2c40e6b063b7b8a2f19
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2024-12-04 23:33:37 +0200

    Updates

--- ProjectCard.jsx
+++ ProjectCard.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import styles from "..//src/sections/Projects/ProjectsStyles.module.css";
 
-function ProjectCard({ src, link, h3 }) {
+function ProjectCard({ src, text, link, h3 }) {
   return (
-    <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
-      <h3>{h3}</h3>
-    </a>
+    <div>
+      <div className={styles.imgcontainer}>
+        <img src={src} alt={` logo`} />
+        <div className={styles.details}>
+          <p>{text}</p>
+          <a href={link} target="_blank">
+            <button>Learn more</button>
+          </a>
+        </div>
+      </div>
+      <a href={link} target="_blank">
+        <h3>{h3}</h3>
+      </a>
+    </div>
   );
 }
 
