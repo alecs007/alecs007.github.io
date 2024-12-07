6e38404bb86f9ebab8b507cd4646ab7cb351f773
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2024-12-07 19:44:47 +0200

    Updated

--- src/sections/Sidebar/Sidebar.jsx
+++ src/sections/Sidebar/Sidebar.jsx
@@ -3,8 +3,11 @@ import styles from "./SidebarStyles.module.css";
 function Sidebar({ onToggleSidebar, isOpen }) {
   return (
     <section className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
-      <a onClick={onToggleSidebar} href={"#projects"}>
+      <a onClick={onToggleSidebar} href={"#hero"}>
         Home
+      </a>
+      <a onClick={onToggleSidebar} href={"#projects"}>
+        Projects
       </a>
     </section>
   );
